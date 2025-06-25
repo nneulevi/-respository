bf443dfeb3c1b5124e796b043b7e16b6e44dcde2
starflowrain <kiyotaka_2022@qq.com>
2025-06-25 17:20:20 +0800

    Merge branch 'main' of https://github.com/nneulevi/-respository

--- src/main/java/org/example/project2/Service/AIService.java
+++ src/main/java/org/example/project2/Service/AIService.java
@@ -1,16 +1,28 @@
 package org.example.project2.Service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
 @Service
 public class AIService {
-    //private final ChatClient chatClient;
+    private final OpenAiChatModel model;
 
-    /*public AIService(ChatClient chatClient) {
-        this.chatClient = chatClient;
+    public AIService(OpenAiChatModel model) {
+        this.model = model;
     }
 
     public String chat(String input) {
-        return chatClient.call(input);  // 调用 OpenAI API
-    }*/
+        Prompt prompt = new Prompt(new UserMessage(input));
+
+        AssistantMessage reply = (AssistantMessage) model.call(prompt)
+                .getResults()
+                .get(0)
+                .getOutput();
+
+        return reply.getText();
+    }
 }
--- src/main/java/org/example/project2/config/AIconfig.java
+++ src/main/java/org/example/project2/config/AIconfig.java
@@ -1,13 +1,18 @@
 package org.example.project2.config;
 
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.ChatClient;
 
+import java.util.Map;
+
 
 @Configuration
 public class AIconfig {
-
+    @Value("${openai.api-key}")
+    private String openAiApiKey;
 }
