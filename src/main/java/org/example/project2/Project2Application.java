24c98f54e998f6c0724ad5d2f5003785037a90d0
starflowrain <kiyotaka_2022@qq.com>
2025-06-30 19:20:01 +0800

    Merge branch 'main' of https://github.com/nneulevi/-respository

--- src/main/java/org/example/project2/Project2Application.java
+++ src/main/java/org/example/project2/Project2Application.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project2Application {
+    static {
+        System.setProperty("http.proxyHost", "127.0.0.1");
+        System.setProperty("http.proxyPort", "7890");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "7890");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Project2Application.class, args);
