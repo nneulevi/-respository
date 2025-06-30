6d04d8752822b1fb630924462ed485f03aea31f2
nneulevi <160100620+nneulevi@users.noreply.github.com>
2025-06-30 16:09:10 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- 只保留 router-view -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+  // 不需要手动导入和注册 Login 组件
+  // Router 会自动处理组件渲染
+}
+</script>
+
+<style>
+/* 可以添加全局样式 */
+#app {
+  height: 100vh;
+}
+
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+</style>
+
+
